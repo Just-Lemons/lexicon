f72e7892cc06cbc770e3c7647c0ee7df7b706835
Geethika <april.appy24@gmail.com>
2026-04-10 01:02:34 +0530

    Simple prototype(Without burpsuite)

--- webapp/dashboard/views.py
+++ webapp/dashboard/views.py
@@ -1,3 +1,16 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def scan_view(request):
+    url = request.GET.get("url")
+
+    data = None
+
+    if url:
+        res = requests.get("http://127.0.0.1:8001/scan", params={"url": url})
+        data = res.json()
+
+    return render(request, "dashboard.html", {"data": data})
