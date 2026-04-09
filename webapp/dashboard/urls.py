b87bdb622e15ab27a101939031c08a7ccacea277
MinMin <manchalasrinu05@gmail.com>
2026-04-10 02:07:01 +0530

    UI

--- webapp/dashboard/urls.py
+++ webapp/dashboard/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('scan/', views.scan_view, name='scan'),
+    path('scan/', views.output_view, name='scan'),
 ]
